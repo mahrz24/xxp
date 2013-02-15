b3d1c4e2a6d4c680d8c8aa43ac7dd642edbb6547
Malte Harder <malte.harder@gmail.com>
2013-02-15 16:19:07 +0100

    [core] Experiment state data type defined

--- core/lib-src/Xxp/Core.hs
+++ core/lib-src/Xxp/Core.hs
@@ -8,26 +8,74 @@ import Control.Monad.Trans.State
 import Control.Monad
 import Control.Monad.IO.Class
 
-data ST = ST Int
+import Data.Maybe
+import Data.List
+import Data.Time.Clock
+import Data.UUID.V1
+import qualified Data.UUID as UUID
+
+import Text.JSON
+
+import System.Environment
+import System.FilePath
+import System.Log.Logger
+
+data LoggingState = LoggingState { dataLogLocation :: Maybe FilePath
+                                 , mirrorDataLogsInRunLogs :: Bool
+                                 , fileLogLevel :: Priority
+                                   -- Passed from xxp binary 
+                                   -- concerns the runtime log level 
+                                   -- for stdout
+                                 , consoleLogLevel :: Priority
+                                   -- Keep track of all data log files 
+                                 , dataLogFiles :: [String]
+                                 } deriving (Show, Eq)
+                    
+data ST = ST { experimentName :: String
+             , tag :: String
+             , uuid :: String
+             , timestamp ::  UTCTime
+             , loggingState :: LoggingState
+             , experimentConfig :: String
+             } deriving (Show,Eq)
 
 type XXP = StateT ST IO
 
-initialState = ST 0
+initialState :: IO ST
+initialState = do
+  time <- getCurrentTime
+  name <- getProgName
+  args <- getArgs
+  gduuid <- nextUUID
+  let l = LoggingState { dataLogLocation = Nothing
+                      , mirrorDataLogsInRunLogs = False
+                      , fileLogLevel = INFO
+                      , consoleLogLevel = WARNING
+                      , dataLogFiles = []
+                      }
+  return $ ST { experimentName = name
+              , tag = args !! 0
+              , uuid = fromMaybe "" (fmap UUID.toString gduuid)
+              , timestamp = time
+              , loggingState = l
+              , experimentConfig = ""
+              }
 
 loadConfiguration :: XXP ()
-loadConfiguration = do put $ ST 1
-                       return ()
+loadConfiguration = return ()
 
 spawn :: XXP ()
-spawn = do (ST x) <- get
-           liftIO $ putStrLn (show x)
-           return ()
+spawn = return ()
 
 runXXP :: XXP () -> IO ()
-runXXP xp = liftM fst $ runStateT (wrapExperiment xp) initialState
+runXXP xp = do 
+  state <- initialState
+  liftM fst $ runStateT (wrapExperiment xp) state
 
 wrapExperiment :: XXP () -> XXP ()
 wrapExperiment xp = do
+  st <- get
+  liftIO $ putStrLn (show st)
   -- Get Arguments
   -- Get Timestamp
   -- Get UUID
