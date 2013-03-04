5313b1882d7edc957c4ddf06f8840c154a8918c8
Malte Harder <malte.harder@gmail.com>
2013-03-04 10:22:02 +0100

    Small change in xp file

--- example/xp_test1.hs
+++ example/xp_test1.hs
@@ -6,6 +6,7 @@ Short TODO list of what I want to implement for a initial version
 
 * Minimal data logging facilities via domain sockets
 * List & less for data logs
+* Pass data/log directory to binary
 
 -}
 
